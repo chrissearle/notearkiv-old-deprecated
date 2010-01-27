e503cdb2db4db3ef77ad11a4f427627321f62386
Chris Searle <chris@chrissearle.org>
2010-01-27 11:26:45 +0100

    Add voice as string

--- db/migrate/20100125133631_create_notes.rb
+++ db/migrate/20100125133631_create_notes.rb
@@ -1,11 +1,11 @@
 class CreateNotes < ActiveRecord::Migration
   def self.up
     create_table :notes do |t|
-      t.int :display_id
+      t.integer :display_id
       t.string :title
-      t.int :count_originals
-      t.int :count_copies
-      t.int :count_instrumental
+      t.integer :count_originals
+      t.integer :count_copies
+      t.integer :count_instrumental
 
       t.timestamps
     end
