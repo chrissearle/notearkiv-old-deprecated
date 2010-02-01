7000eb8de37321519a3e66f0f538be6962e5d93d
Chris Searle <chris@chrissearle.org>
2010-02-01 15:21:40 +0100

    Initial basic excel generation

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,5 @@
+require 'spreadsheet'
+
 class NotesController < ApplicationController
 
   def index
@@ -7,6 +9,30 @@ class NotesController < ApplicationController
       format.html # index.html.erb
       format.xml  { render :xml => @notes }
     end
+  end
+
+  def excel
+    book = Spreadsheet::Workbook.new
+    sheet1 = book.create_worksheet
+    sheet1.name = "Notearkiv"
+    sheet1.row(0).concat %w{ID Tittel Original Kopi Instrumental Besetning}
+
+    Note.find(:all).each do |note|
+      row = sheet1.row(sheet1.last_row_index() + 1)
+
+      row.push note.display_id
+      row.push note.title
+      row.push note.count_originals
+      row.push note.count_copies
+      row.push note.count_instrumental
+      row.push note.voice
+    end
+
+    tmp_file = Tempfile.new('notearkiv')
+
+    book.write(tmp_file)
+    
+    send_file tmp_file.path, :filename => 'notearkiv.xls', :type => 'application/vnd.ms-excel', :disposition => 'attachment'
   end
 
 end
