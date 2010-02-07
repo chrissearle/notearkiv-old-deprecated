2388e704e64b8bc1e59a94acf624813c85c24c0c
Chris Searle <chris@chrissearle.org>
2010-02-07 19:01:12 +0100

    Move keys to environment

--- app/helpers/evensongs_helper.rb
+++ app/helpers/evensongs_helper.rb
@@ -5,8 +5,8 @@ module EvensongsHelper
 
     if (evensong.url.nil? || evensong.url == "")
       if (check)
-        db = DropBox.new('chris+notearkiv.ochs.no@chrissearle.org',
-                         'bei1Ahng',
+        db = DropBox.new(ENV['DROPBOX_USER'],
+                         ENV['DROPBOX_PASS'],
                          'Public/Evensong')
 
         file = db.list.find {|f| f["name"] == "evensong_#{evensong.id}.pdf" }
