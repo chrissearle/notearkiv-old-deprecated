ca8ad827d369f8ca09f0ae4ace9652e8b6f8ddcb
Chris Searle <chris@chrissearle.org>
2010-02-17 13:38:05 +0100

    Add epoke to note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,7 @@
 class Note < ActiveRecord::Base
   belongs_to :composer
   belongs_to :genre
+  belongs_to :period
 
   validates_presence_of :item, :title, :voice
 end
---
+++ db/migrate/20100217123523_add_period_to_note.rb
@@ -0,0 +1,9 @@
+class AddPeriodToNote < ActiveRecord::Migration
+  def self.up
+    add_column :notes, :period_id, :integer
+  end
+
+  def self.down
+    remove_column :notes, :period_id
+  end
+end
