107d68f255147f9ee0b839d42046626f90bd4e83
Chris Searle <chris@chrissearle.org>
2010-02-18 10:23:46 +0100

    Change to .gems manifest for heroku

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,7 +1,6 @@
 authorization do
   role :admin do
     has_permission_on [:notes, :evensongs, :composers, :genres, :periods, :languages, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :excel]
-    has_permission_on :authorization_rules, :to => :read
   end
 
   role :normal do
