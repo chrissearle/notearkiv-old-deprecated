3934c5eda7d5f2e230488083e06b9e30ac699d76
Chris Searle <chris@chrissearle.org>
2010-02-20 22:07:05 +0100

    Don't update file link on show.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,5 @@
 module ApplicationHelper
-  def link_to_dropbox_file(object, type, check)
-
-    if (object.url.nil? || object.url == "")
-      if (check)
-        object.update_link
-      end
-    end
-
+  def link_to_dropbox_file(object)
     if (object.url)
       s = %{
       <a href="#{object.url}">PDF</a>
@@ -14,13 +7,5 @@ module ApplicationHelper
     else
       s = "Ikke tilgjengelig"
     end
-  end
-
-  def link_to_evensong_file(evensong, check = false)
-    link_to_dropbox_file(evensong, :evensong_archive, check)
-  end
-
-  def link_to_note_file(note, check = false)
-    link_to_dropbox_file(note, :note_file, check)
   end
 end
