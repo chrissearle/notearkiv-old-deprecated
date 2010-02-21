2f9c0b93f780c9eba013b5f1995b29bafa612deb
Chris Searle <chris@chrissearle.org>
2010-02-21 15:49:00 +0100

    Remove old_file_path from db

---
+++ db/migrate/20100221144728_remove_old_file_path_from_evensongs.rb
@@ -0,0 +1,9 @@
+class RemoveOldFilePathFromEvensongs < ActiveRecord::Migration
+  def self.up
+    remove_column :evensongs, :old_file_path
+  end
+
+  def self.down
+    add_column :evensongs, :old_file_path, :string
+  end
+end
