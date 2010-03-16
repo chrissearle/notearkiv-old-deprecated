da326edc7d0bb06fcc5b158a21bb740e3471d4e7
Chris Searle <chris@chrissearle.org>
2010-03-16 14:06:10 +0100

    Tidy auth rules

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -4,10 +4,10 @@ authorization do
   end
 
   role :admin do
-    has_permission_on [:notes, :evensongs, :composers, :genres, :periods, :languages], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :excel, :voice]
+    has_permission_on [:notes, :evensongs, :composers, :genres, :periods, :languages], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :voice]
   end
 
   role :normal do
-    has_permission_on [:notes, :evensongs, :composers, :genres, :periods, :languages], :to => [:index, :show, :excel, :voice, :voices]
+    has_permission_on [:notes, :evensongs, :composers, :genres, :periods, :languages], :to => [:index, :show]
   end
 end
