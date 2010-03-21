4d60e58a1ea5029d8ed0edb1a64487fc9b72a7b1
Chris Searle <chris@chrissearle.org>
2010-03-21 10:21:45 +0100

    Add filter_access_to

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,8 @@
 require 'archive/archive'
 
 class StatsController < ApplicationController
+  filter_access_to :all
+
   def index
     # We can use any archive type - stats are global
     archive = Archive.new :note_archive, :document
