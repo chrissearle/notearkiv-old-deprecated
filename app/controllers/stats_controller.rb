dad47be596ba54e839fcf8dafcfc2bea08a7b8da
Chris Searle <chris@chrissearle.org>
2010-04-16 22:34:43 +0200

    Update to dropbox 0.0.9 - gives a few more stats.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,14 +7,7 @@ class StatsController < ApplicationController
     # We can use any archive type - stats are global
     archive = Archive.new :note_archive, :document
 
-    stats = archive.stats
-
-    # In 0.0.6 dropbox gem used is coming back in bytes not gb's and the free calculation is not taking account of this
-
-    @percent = stats[:percent]
-    @total = stats[:total]
-    @used = stats[:used] / (1024*1024)
-    @free = @total - @used
+    @stats = archive.stats
   end
 
 end
