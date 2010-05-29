70c0ad31f8710a6cce0d1f9f9905d914630af928
Chris Searle <chris@chrissearle.org>
2010-05-29 19:51:09 +0200

    Spec test stats controller

---
+++ spec/controllers/stats_controller_spec.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe StatsController do
+  before(:each) do
+    Authorization.ignore_access_control(true)
+  end
+
+  describe "GET index" do
+    it "should respond and ask for stats" do
+      archive_connection = mock_model(ArchiveConnection)
+      ArchiveConnection.stub(:new).and_return(archive_connection)
+      archive_connection.should_receive(:stats)
+      
+      get :index
+    end
+  end
+end
