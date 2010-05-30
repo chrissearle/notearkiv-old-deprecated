70a8ab19516ba1b068bd0a711001b9b61c36d1cc
Chris Searle <chris@chrissearle.org>
2010-05-30 09:16:38 +0200

    Test one_time_code

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe User do
+  fixtures :users
+
+  it "should generate a one time code" do
+    user = User.find(:first)
+
+    code = user.one_time_code
+
+    code.should be_true(code.size == 40)
+  end
+
+  it "should send a message" do
+    arkiv = mock(Arkiv)
+
+    user = User.find(:first)
+
+    user.clear_one_time_code
+
+    user.send_reset_password
+
+    user.onetime.should_not be_nil
+  end
+
+end
